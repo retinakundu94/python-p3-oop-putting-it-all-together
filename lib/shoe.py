f3086d7cc8aa745f57a4ec60a46a12688cd09b99
Retina Kundu <retinakundu94@gmail.com>
2024-05-02 14:22:54 -0400

    add changes

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand: str, size: int):
+        self.brand = brand
+        self._size = size
+        self.is_repaired = False
+        self.condition = "Used"  
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value: int):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.is_repaired = True
+        self.condition = "New"  
+        print("Your shoe is as good as new!")
